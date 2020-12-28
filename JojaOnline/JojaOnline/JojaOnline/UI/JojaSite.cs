51522c7fc936818abd9be043830e0c2a688e4333
Mr. Automation <31755155+Floogen@users.noreply.github.com>
2020-12-27 18:14:15 -0600

    UI only implementation of the Joja Site

--- JojaOnline/JojaOnline/JojaOnline/UI/JojaSite.cs
+++ JojaOnline/JojaOnline/JojaOnline/UI/JojaSite.cs
@@ -14,6 +14,7 @@ namespace JojaOnline.JojaOnline.UI
     public class JojaSite: IClickableMenu
     {
         private readonly float scale = 1f;
+        private readonly List<ClickableTextureComponent> clickables = new List<ClickableTextureComponent>();
 
         public JojaSite(int uiWidth, int uiHeight) : base(Game1.uiViewport.Width / 2 - (uiWidth + IClickableMenu.borderWidth * 2) / 2, Game1.uiViewport.Height / 2 - (uiHeight + IClickableMenu.borderWidth * 2) / 2, uiWidth + IClickableMenu.borderWidth * 2, uiHeight + IClickableMenu.borderWidth * 2, showUpperRightCloseButton: true)
         {
@@ -25,7 +26,19 @@ namespace JojaOnline.JojaOnline.UI
                 this.height = (int)(this.height * scale);
             }
 
-            this.initializeUpperRightCloseButton();
+            // Draw the clickables (buttons, etc)
+            Texture2D sourceSheet = JojaResources.GetJojaSiteSpriteSheet();
+
+            // Joja Ads
+            drawClickable("jojaCanAd", 42, 248, sourceSheet, new Rectangle(608, 0, 208, 208));
+            drawClickable("jojaJoinUsAd", 829, 260, sourceSheet, new Rectangle(0, 352, 201, 194));
+
+            // Banner, logos
+            drawClickable("jojaLogo", 35, 105, sourceSheet, new Rectangle(0, 0, 336, 128));
+            drawClickable("jojaMotto", 435, 150, sourceSheet, new Rectangle(0, 144, 384, 64));
+            drawClickable("shoppingCart", 900, 150, sourceSheet, new Rectangle(0, 208, 80, 48));
+
+            // Override default close button position
             this.upperRightCloseButton = new ClickableTextureComponent(new Rectangle(this.xPositionOnScreen + this.width - 50, this.yPositionOnScreen + 70, 48, 48), Game1.mouseCursors, new Rectangle(337, 494, 12, 12), 4f);
         }
 
@@ -39,11 +52,34 @@ namespace JojaOnline.JojaOnline.UI
         {
             b.Draw(Game1.fadeToBlackRect, Game1.graphics.GraphicsDevice.Viewport.Bounds, Color.Black * 0.75f);
 
-            Game1.drawDialogueBox(this.xPositionOnScreen, this.yPositionOnScreen, this.width, this.height, speaker: false, drawOnlyBox: true);
+            Game1.drawDialogueBox(this.xPositionOnScreen, this.yPositionOnScreen, this.width, this.height, speaker: false, drawOnlyBox: true, r: 80, g: 123, b:186);
             Game1.dayTimeMoneyBox.drawMoneyBox(b);
+
+            // Draw the custom store BG
+            Rectangle bounds = new Rectangle(this.xPositionOnScreen + 32, this.yPositionOnScreen + 100, this.width, this.height);
+            ClickableTextureComponent bg = new ClickableTextureComponent(bounds, JojaResources.GetJojaSiteBackground(), new Rectangle(0, 0, 1008, 1194), scale)
+            {
+                name = "background"
+            };
+            bg.draw(b);
+            foreach (ClickableTextureComponent clickable in clickables)
+            {
+                clickable.draw(b);
+            }
 
             this.upperRightCloseButton.draw(b);
             this.drawMouse(b);
+        }
+
+        public void drawClickable(string identifier, int x, int y, Texture2D sourceTexture, Rectangle sourceRect)
+        {
+            Rectangle bounds = new Rectangle((int)(this.xPositionOnScreen + x * scale), (int)(this.yPositionOnScreen + y * scale), (int)(sourceRect.Width * scale), (int)(sourceRect.Height * scale));
+            ClickableTextureComponent clickable = new ClickableTextureComponent(bounds, sourceTexture, sourceRect, scale)
+            {
+                name = identifier
+            };
+
+            clickables.Add(clickable);
         }
     }
 }
