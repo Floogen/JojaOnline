901fff61ac1c95465274f84e43fdaffde3caa7d9
Mr. Automation <31755155+Floogen@users.noreply.github.com>
2020-12-27 18:13:50 -0600

    Implemented JojaResource class
    Added resources required for the store page

---
+++ JojaOnline/JojaOnline/JojaOnline/JojaResources.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using StardewModdingAPI;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JojaOnline
+{
+    public static class JojaResources
+    {
+        private static Texture2D jojaMailBackground;
+        private static Texture2D jojaSiteBackground;
+        private static Texture2D jojaSiteSpriteSheet;
+
+        public static void LoadTextures(IModHelper helper)
+        {
+            // Load the MFM related background(s)
+            jojaMailBackground = helper.Content.Load<Texture2D>(Path.Combine("assets", "jojaLetterBG.png"));
+
+            // Load in the JojaSite background
+            jojaSiteBackground = helper.Content.Load<Texture2D>(Path.Combine("assets", "jojaStoreBG.png"));
+
+            // Load in the JojaSite spritesheet
+            jojaSiteSpriteSheet = helper.Content.Load<Texture2D>(Path.Combine("assets", "jojaSiteSprites.png"));
+        }
+
+        public static Texture2D GetJojaMailBackground()
+        {
+            return jojaMailBackground;
+        }
+
+        public static Texture2D GetJojaSiteBackground()
+        {
+            return jojaSiteBackground;
+        }
+
+        public static Texture2D GetJojaSiteSpriteSheet()
+        {
+            return jojaSiteSpriteSheet;
+        }
+    }
+}
