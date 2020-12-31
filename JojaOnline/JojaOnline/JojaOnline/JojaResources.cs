8d816534fc64a87fd7f309cfd9b13eb953c41ff7
Mr. Automation <31755155+Floogen@users.noreply.github.com>
2020-12-30 19:59:30 -0600

    Reworked SetJojaOnlineStock and items sold

--- JojaOnline/JojaOnline/JojaOnline/JojaResources.cs
+++ JojaOnline/JojaOnline/JojaOnline/JojaResources.cs
@@ -2,9 +2,11 @@
 using Microsoft.Xna.Framework.Graphics;
 using StardewModdingAPI;
 using StardewValley;
+using StardewValley.Locations;
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using Object = StardewValley.Object;
 
 namespace JojaOnline
@@ -37,7 +39,59 @@ namespace JojaOnline
 		public static void SetJojaOnlineStock()
 		{
 			// Clone the current stock
-			jojaOnlineStock = Utility.getJojaStock();
+			jojaOnlineStock = new Dictionary<ISalable, int[]>();
+
+			// Add wood, stone and hardwood
+			AddToJojaOnlineStock(new Object(Vector2.Zero, 388, int.MaxValue));
+			AddToJojaOnlineStock(new Object(Vector2.Zero, 390, int.MaxValue));
+			AddToJojaOnlineStock(new Object(Vector2.Zero, 390, int.MaxValue), 500);
+
+			// Add coal
+			AddToJojaOnlineStock(new Object(Vector2.Zero, 382, int.MaxValue));
+
+			// Add battery pack
+			AddToJojaOnlineStock(new Object(Vector2.Zero, 390, int.MaxValue), 2500);
+
+			// Add cloth
+			AddToJojaOnlineStock(new Object(Vector2.Zero, 428, int.MaxValue), 2000);
+
+			// Add energy tonic
+			AddToJojaOnlineStock(new Object(Vector2.Zero, 349, int.MaxValue));
+
+			// Add some of Pierre's goods (if available)
+			foreach (Item item in (new SeedShop()).shopStock().Keys)
+            {
+				if (item.parentSheetIndex == 368)
+                {
+					AddToJojaOnlineStock(new Object(Vector2.Zero, item.parentSheetIndex, int.MaxValue), 100);
+				}
+				else if (item.parentSheetIndex == 369)
+				{
+					AddToJojaOnlineStock(new Object(Vector2.Zero, item.parentSheetIndex, int.MaxValue), 150);
+				}
+				else if (item.parentSheetIndex == 370)
+				{
+					AddToJojaOnlineStock(new Object(Vector2.Zero, item.parentSheetIndex, int.MaxValue), 100);
+				}
+				else if (item.parentSheetIndex == 371)
+				{
+					AddToJojaOnlineStock(new Object(Vector2.Zero, item.parentSheetIndex, int.MaxValue), 150);
+				}
+				else if (item.parentSheetIndex == 465)
+				{
+					AddToJojaOnlineStock(new Object(Vector2.Zero, item.parentSheetIndex, int.MaxValue), 100);
+				}
+				else if (item.parentSheetIndex == 466)
+				{
+					AddToJojaOnlineStock(new Object(Vector2.Zero, item.parentSheetIndex, int.MaxValue), 150);
+				}
+			}
+
+			// Add some of Marnie's goods (only hay for now)
+			AddToJojaOnlineStock(new Object(Vector2.Zero, 178, int.MaxValue), 50);
+
+			// Add the current JojaMart items
+			Utility.getJojaStock().ToList().ForEach(x => jojaOnlineStock[x.Key] = x.Value);
 
 			// If past year one, unlock all seeds (that aren't in the current season due to initial cloning)
 			if (Game1.year > 1)
@@ -45,125 +99,44 @@ namespace JojaOnline
 				modMonitor.Log("Loading JojaMart's stock for all seasons for JojaOnline", LogLevel.Debug);
 				if (!Game1.currentSeason.Equals("spring"))
 				{
-					AddToJojaOnlineStock(new Object(Vector2.Zero, 472, int.MaxValue), new int[2]
-					{
-						(int)((float)Convert.ToInt32(Game1.objectInformation[472].Split('/')[1])), 2147483647
-					});
-					AddToJojaOnlineStock(new Object(Vector2.Zero, 473, int.MaxValue), new int[2]
-					{
-						(int)((float)Convert.ToInt32(Game1.objectInformation[473].Split('/')[1])), 2147483647
-					});
-					AddToJojaOnlineStock(new Object(Vector2.Zero, 474, int.MaxValue), new int[2]
-					{
-						(int)((float)Convert.ToInt32(Game1.objectInformation[474].Split('/')[1])), 2147483647
-					});
-					AddToJojaOnlineStock(new Object(Vector2.Zero, 475, int.MaxValue), new int[2]
-					{
-						(int)((float)Convert.ToInt32(Game1.objectInformation[475].Split('/')[1])), 2147483647
-					});
-					AddToJojaOnlineStock(new Object(Vector2.Zero, 427, int.MaxValue), new int[2]
-					{
-						(int)((float)Convert.ToInt32(Game1.objectInformation[427].Split('/')[1])), 2147483647
-					});
-					AddToJojaOnlineStock(new Object(Vector2.Zero, 429, int.MaxValue), new int[2]
-					{
-						(int)((float)Convert.ToInt32(Game1.objectInformation[429].Split('/')[1])), 2147483647
-					});
-					AddToJojaOnlineStock(new Object(Vector2.Zero, 477, int.MaxValue), new int[2]
-					{
-						(int)((float)Convert.ToInt32(Game1.objectInformation[477].Split('/')[1])), 2147483647
-					});
+					AddToJojaOnlineStock(new Object(Vector2.Zero, 472, int.MaxValue));
+					AddToJojaOnlineStock(new Object(Vector2.Zero, 473, int.MaxValue));
+					AddToJojaOnlineStock(new Object(Vector2.Zero, 474, int.MaxValue));
+					AddToJojaOnlineStock(new Object(Vector2.Zero, 475, int.MaxValue));
+					AddToJojaOnlineStock(new Object(Vector2.Zero, 427, int.MaxValue));
+					AddToJojaOnlineStock(new Object(Vector2.Zero, 429, int.MaxValue));
+					AddToJojaOnlineStock(new Object(Vector2.Zero, 477, int.MaxValue));
 				}
 				if (!Game1.currentSeason.Equals("summer"))
 				{
-					AddToJojaOnlineStock(new Object(Vector2.Zero, 480, int.MaxValue), new int[2]
-					{
-						(int)((float)Convert.ToInt32(Game1.objectInformation[480].Split('/')[1])), 2147483647
-					});
-					AddToJojaOnlineStock(new Object(Vector2.Zero, 482, int.MaxValue), new int[2]
-					{
-						(int)((float)Convert.ToInt32(Game1.objectInformation[482].Split('/')[1])), 2147483647
-					});
-					AddToJojaOnlineStock(new Object(Vector2.Zero, 483, int.MaxValue), new int[2]
-					{
-						(int)((float)Convert.ToInt32(Game1.objectInformation[483].Split('/')[1])), 2147483647
-					});
-					AddToJojaOnlineStock(new Object(Vector2.Zero, 484, int.MaxValue), new int[2]
-					{
-						(int)((float)Convert.ToInt32(Game1.objectInformation[484].Split('/')[1])), 2147483647
-					});
-					AddToJojaOnlineStock(new Object(Vector2.Zero, 479, int.MaxValue), new int[2]
-					{
-						(int)((float)Convert.ToInt32(Game1.objectInformation[479].Split('/')[1])), 2147483647
-					});
-					AddToJojaOnlineStock(new Object(Vector2.Zero, 302, int.MaxValue), new int[2]
-					{
-						(int)((float)Convert.ToInt32(Game1.objectInformation[302].Split('/')[1])), 2147483647
-					});
-					AddToJojaOnlineStock(new Object(Vector2.Zero, 453, int.MaxValue), new int[2]
-					{
-						(int)((float)Convert.ToInt32(Game1.objectInformation[453].Split('/')[1])), 2147483647
-					});
-					AddToJojaOnlineStock(new Object(Vector2.Zero, 455, int.MaxValue), new int[2]
-					{
-						(int)((float)Convert.ToInt32(Game1.objectInformation[455].Split('/')[1])), 2147483647
-					});
-					AddToJojaOnlineStock(new Object(431, int.MaxValue, isRecipe: false, 100), new int[2]
-					{
-						(int)(50f), 2147483647
-					});
+					AddToJojaOnlineStock(new Object(Vector2.Zero, 480, int.MaxValue));
+					AddToJojaOnlineStock(new Object(Vector2.Zero, 482, int.MaxValue));
+					AddToJojaOnlineStock(new Object(Vector2.Zero, 483, int.MaxValue));
+					AddToJojaOnlineStock(new Object(Vector2.Zero, 484, int.MaxValue));
+					AddToJojaOnlineStock(new Object(Vector2.Zero, 479, int.MaxValue));
+					AddToJojaOnlineStock(new Object(Vector2.Zero, 302, int.MaxValue));
+					AddToJojaOnlineStock(new Object(Vector2.Zero, 453, int.MaxValue));
+					AddToJojaOnlineStock(new Object(Vector2.Zero, 455, int.MaxValue));
+					AddToJojaOnlineStock(new Object(431, int.MaxValue, isRecipe: false, 100));
 				}
 				if (!Game1.currentSeason.Equals("fall"))
 				{
-					AddToJojaOnlineStock(new Object(Vector2.Zero, 487, int.MaxValue), new int[2]
-					{
-						(int)((float)Convert.ToInt32(Game1.objectInformation[487].Split('/')[1])), 2147483647
-					});
-					AddToJojaOnlineStock(new Object(Vector2.Zero, 488, int.MaxValue), new int[2]
-					{
-						(int)((float)Convert.ToInt32(Game1.objectInformation[488].Split('/')[1])), 2147483647
-					});
-					AddToJojaOnlineStock(new Object(Vector2.Zero, 483, int.MaxValue), new int[2]
-					{
-						(int)((float)Convert.ToInt32(Game1.objectInformation[483].Split('/')[1])), 2147483647
-					});
-					AddToJojaOnlineStock(new Object(Vector2.Zero, 490, int.MaxValue), new int[2]
-					{
-						(int)((float)Convert.ToInt32(Game1.objectInformation[490].Split('/')[1])), 2147483647
-					});
-					AddToJojaOnlineStock(new Object(Vector2.Zero, 299, int.MaxValue), new int[2]
-					{
-						(int)((float)Convert.ToInt32(Game1.objectInformation[299].Split('/')[1])), 2147483647
-					});
-					AddToJojaOnlineStock(new Object(Vector2.Zero, 301, int.MaxValue), new int[2]
-					{
-						(int)((float)Convert.ToInt32(Game1.objectInformation[301].Split('/')[1])), 2147483647
-					});
-					AddToJojaOnlineStock(new Object(Vector2.Zero, 492, int.MaxValue), new int[2]
-					{
-						(int)((float)Convert.ToInt32(Game1.objectInformation[492].Split('/')[1])), 2147483647
-					});
-					AddToJojaOnlineStock(new Object(Vector2.Zero, 491, int.MaxValue), new int[2]
-					{
-						(int)((float)Convert.ToInt32(Game1.objectInformation[491].Split('/')[1])), 2147483647
-					});
-					AddToJojaOnlineStock(new Object(Vector2.Zero, 493, int.MaxValue), new int[2]
-					{
-						(int)((float)Convert.ToInt32(Game1.objectInformation[493].Split('/')[1])), 2147483647
-					});
-					AddToJojaOnlineStock(new Object(431, int.MaxValue, isRecipe: false, 100), new int[2]
-					{
-						(int)(50f), 2147483647
-					});
-					AddToJojaOnlineStock(new Object(Vector2.Zero, 425, int.MaxValue), new int[2]
-					{
-						(int)((float)Convert.ToInt32(Game1.objectInformation[425].Split('/')[1])), 2147483647
-					});
+					AddToJojaOnlineStock(new Object(Vector2.Zero, 487, int.MaxValue));
+					AddToJojaOnlineStock(new Object(Vector2.Zero, 488, int.MaxValue));
+					AddToJojaOnlineStock(new Object(Vector2.Zero, 483, int.MaxValue));
+					AddToJojaOnlineStock(new Object(Vector2.Zero, 490, int.MaxValue));
+					AddToJojaOnlineStock(new Object(Vector2.Zero, 299, int.MaxValue));
+					AddToJojaOnlineStock(new Object(Vector2.Zero, 301, int.MaxValue));
+					AddToJojaOnlineStock(new Object(Vector2.Zero, 492, int.MaxValue));
+					AddToJojaOnlineStock(new Object(Vector2.Zero, 491, int.MaxValue));
+					AddToJojaOnlineStock(new Object(Vector2.Zero, 493, int.MaxValue));
+					AddToJojaOnlineStock(new Object(431, int.MaxValue, isRecipe: false, 100));
+					AddToJojaOnlineStock(new Object(Vector2.Zero, 425, int.MaxValue));
 				}
 			}
 		}
 
-		public static void AddToJojaOnlineStock(Object item, int[] details)
+		public static void AddToJojaOnlineStock(Object item, int salePrice = -1, int stock = -1)
         {
 			if (cachedItemSheetIndexes.Contains(item.parentSheetIndex))
             {
@@ -172,7 +145,7 @@ namespace JojaOnline
 			cachedItemSheetIndexes.Add(item.parentSheetIndex);
 
 			// Add the unique item
-			jojaOnlineStock.Add(item, details);
+			jojaOnlineStock.Add(item, new int[] { salePrice == -1 ? item.salePrice() : salePrice, stock == -1 ? int.MaxValue : stock });
 		}
 
 		public static Dictionary<ISalable, int[]> GetJojaOnlineStock()
