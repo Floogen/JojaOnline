94ce6e90108ac0a65877ec951654274b95462bf8
Mr. Automation <31755155+Floogen@users.noreply.github.com>
2021-01-03 10:10:10 -0600

    Created ModConfig class

---
+++ JojaOnline/JojaOnline/JojaOnline/ModConfig.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JojaOnline.JojaOnline
+{
+    public class ModConfig
+    {
+        public bool areAllSeedsAvailableBeforeYearOne { get; set; }
+        public int minSalePercentage { get; set; }
+        public int maxSalePercentage { get; set; }
+
+        public ModConfig()
+        {
+            this.areAllSeedsAvailableBeforeYearOne = false;
+            this.minSalePercentage = 5;
+            this.maxSalePercentage = 35;
+        }
+    }
+}
