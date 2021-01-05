906025c2859b8218e87adbb9f5d93a272e3f9d46
Mr. Automation <31755155+Floogen@users.noreply.github.com>
2021-01-04 20:41:45 -0600

    Created giveJojaMemberDiscount and itemNameToPriceOverrides

--- JojaOnline/JojaOnline/JojaOnline/ModConfig.cs
+++ JojaOnline/JojaOnline/JojaOnline/ModConfig.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using StardewValley;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,14 +10,21 @@ namespace JojaOnline.JojaOnline
     public class ModConfig
     {
         public bool areAllSeedsAvailableBeforeYearOne { get; set; }
+        public bool giveJojaMemberDiscount { get; set; }
         public int minSalePercentage { get; set; }
         public int maxSalePercentage { get; set; }
+
+        public Dictionary<string, int> itemNameToPriceOverrides { get; set; }
 
         public ModConfig()
         {
             this.areAllSeedsAvailableBeforeYearOne = false;
+            this.giveJojaMemberDiscount = false;
+
             this.minSalePercentage = 5;
             this.maxSalePercentage = 35;
+
+            this.itemNameToPriceOverrides = new Dictionary<string, int>();
         }
     }
 }
