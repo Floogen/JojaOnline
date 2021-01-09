570b5af12fb0edd77a7e3074cca4923c37d4e554
Mr. Automation <31755155+Floogen@users.noreply.github.com>
2021-01-09 14:55:55 -0600

    Implemented interface for JsonAssets

---
+++ JojaOnline/JojaOnline/JojaOnline/API/IJsonAssetApi.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JojaOnline.JojaOnline.API
+{
+    public interface IJsonAssetApi
+    {
+        List<string> GetAllObjectsFromContentPack(string cp);
+        IDictionary<string, int> GetAllObjectIds();
+        int GetObjectId(string name);
+    }
+}
